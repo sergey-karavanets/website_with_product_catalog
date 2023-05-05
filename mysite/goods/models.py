4bce797409be0c5fbb84258b2d08b525e41625d6
Sergey.Karavanets <dront22rus@mail.ru>
2023-05-05 14:22:54 +0700

    add 'Category' and 'Product' models in models.py

--- mysite/goods/models.py
+++ mysite/goods/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, verbose_name='Название категории товара')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория товара'
+        verbose_name_plural = 'Категории товара'
+        ordering = ('name',)
+
+
+class Product(models.Model):
+    vendor_code = models.IntegerField(verbose_name='Артикул')
+    name = models.CharField(max_length=128, verbose_name='Название товара')
+    price = models.PositiveIntegerField(verbose_name='Цена товара')
+    description = models.TextField(max_length=512, verbose_name='Описание товара', blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория товара')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ('name',)
